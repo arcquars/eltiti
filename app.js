14f8c3ab6a3880a5601099ffa5f6f051fe89db65
arc.quars@gmail.com <arc.quars@gmail.com>
2015-05-16 08:48:05 -0400

    archivos de template

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.disable('etag');
+//app.use(express.static(path.join(__dirname, '/javascripts')));
+//app.use(express.static(path.join(__dirname, '/images')));
 
 app.use('/', routes);
 app.use('/users', users);
